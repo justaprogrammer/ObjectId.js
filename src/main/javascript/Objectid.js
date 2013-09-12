43187cd4e482df53d5a60bbdc71400918dc5931e
Justin Dearing <zippy1981@gmail.com>
2013-09-11 20:43:31 -0400

    Fixed unit tests

--- src/main/javascript/Objectid.js
+++ src/main/javascript/Objectid.js
@@ -5,7 +5,7 @@
 * and GPL (http://www.opensource.org/licenses/gpl-license.php) version 2 licenses.
 * This software is not distributed under version 3 or later of the GPL.
 *
-* Version 1.0.0
+* Version 1.0.1-dev
 *
 */
 
@@ -81,9 +81,10 @@ ObjectId.prototype.getDate = function () {
 
 ObjectId.prototype.toArray = function () {
     var strOid = this.toString();
-    var array = new Array();
-    for(var i = 0; i < 12; i++) {
-        array[i] = parseInt(strOid.slice(i*2, i*2+1), 16);
+    var array = [];
+    var i;
+    for(i = 0; i < 12; i++) {
+        array[i] = parseInt(strOid.slice(i*2, i*2+2), 16);
     }
     return array;
 }
