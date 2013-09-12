6e20ceca78d3beed9f6ba7e7705af9c62a3beb67
Justin Dearing <zippy1981@gmail.com>
2013-09-11 20:43:31 -0400

    Fixed unit tests

--- src/test/javascript/Objectid_spec.js
+++ src/test/javascript/Objectid_spec.js
@@ -13,7 +13,7 @@ describe('The ObjectId class',function(){
 			var objectId = new ObjectId(0, 0, 0, 0x00ffffff);
 			var objectIdString = objectId.toString();
 			expect(objectIdString).not.toBe(null);
-			expect(objectId.toString()).toEqual('0000000000000000ffffff');
+			expect(objectId.toString()).toEqual('000000000000000000ffffff');
 		});
 
 		it('should serialize and deserialize the timestamp',function(){
@@ -28,9 +28,11 @@ describe('The ObjectId class',function(){
 
 
 		it('should serialize to a byte array', function(){
-			var array = new Array( 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xa, 0xb, 0xc );
-			var objectId = new ObjectId("0102030405060708090a0b0c");
-			expect(objectId.toArray()).toEqual(array);
+			var oArray = new Array( 0x50, 0x7f, 0x1f, 0x77, 0xbc, 0xf8, 0x6c, 0xd7, 0x99, 0x43, 90, 0x11 );
+			var oString = "507f1f77bcf86cd799439011";
+			var objectId = new ObjectId(oString);
+			expect(objectId.toString()).toEqual(oString);
+			expect(objectId.toArray()).toEqual(oArray);
 		});
 	});
 });
