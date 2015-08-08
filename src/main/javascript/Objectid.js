6613a9955d42768ff75b42ddebcdd0051511730f
Liam Mitchell <liamkarlmitchell@gmail.com>
2015-08-08 21:49:44 +1200

    Allowing passing a string to get ObjectID parsed.
    Returns an ObjectId rather than a string.
    
    Also using !== or === where possible rather than just == or !=.
    
    Sorry I could not find a way to run the tests.

--- src/main/javascript/Objectid.js
+++ src/main/javascript/Objectid.js
@@ -5,8 +5,8 @@
 * and GPL (http://www.opensource.org/licenses/gpl-license.php) version 2 licenses.
 * This software is not distributed under version 3 or later of the GPL.
 *
-* Version 1.0.2
-*
+* Version 1.0.2 (Liams Modifications)
+* Source: https://github.com/justaprogrammer/ObjectId.js
 */
 
 if (!document) var document = { cookie: '' }; // fix crashes on node
@@ -25,14 +25,14 @@ var ObjectId = (function () {
         for (var i in cookieList) {
             var cookie = cookieList[i].split('=');
             var cookieMachineId = parseInt(cookie[1], 10);
-            if (cookie[0] == 'mongoMachineId' && cookieMachineId && cookieMachineId >= 0 && cookieMachineId <= 16777215) {
+            if (cookie[0] === 'mongoMachineId' && cookieMachineId && cookieMachineId >= 0 && cookieMachineId <= 16777215) {
                 machine = cookieMachineId;
                 break;
             }
         }
         document.cookie = 'mongoMachineId=' + machine + ';expires=Tue, 19 Jan 2038 05:00:00 GMT;path=/';
     };
-    if (typeof (localStorage) != 'undefined') {
+    if (typeof (localStorage) !== 'undefined') {
         try {
             var mongoMachineId = parseInt(localStorage['mongoMachineId']);
             if (mongoMachineId >= 0 && mongoMachineId <= 16777215) {
@@ -50,22 +50,23 @@ var ObjectId = (function () {
 
     function ObjId() {
         if (!(this instanceof ObjectId)) {
-            return new ObjectId(arguments[0], arguments[1], arguments[2], arguments[3]).toString();
+            return new ObjectId(arguments[0], arguments[1], arguments[2], arguments[3]);
         }
 
-        if (typeof (arguments[0]) == 'object') {
+        if (typeof (arguments[0]) === 'object') {
             this.timestamp = arguments[0].timestamp;
             this.machine = arguments[0].machine;
             this.pid = arguments[0].pid;
             this.increment = arguments[0].increment;
         }
-        else if (typeof (arguments[0]) == 'string' && arguments[0].length == 24) {
-            this.timestamp = Number('0x' + arguments[0].substr(0, 8)),
-            this.machine = Number('0x' + arguments[0].substr(8, 6)),
-            this.pid = Number('0x' + arguments[0].substr(14, 4)),
-            this.increment = Number('0x' + arguments[0].substr(18, 6))
+        else if (typeof (arguments[0]) === 'string' && arguments[0].length === 24) {
+            debugger;
+            this.timestamp = Number('0x' + arguments[0].substr(0, 8));
+            this.machine = Number('0x' + arguments[0].substr(8, 6));
+            this.pid = Number('0x' + arguments[0].substr(14, 4));
+            this.increment = Number('0x' + arguments[0].substr(18, 6));
         }
-        else if (arguments.length == 4 && arguments[0] != null) {
+        else if (arguments.length === 4 && arguments[0] !== null) {
             this.timestamp = arguments[0];
             this.machine = arguments[1];
             this.pid = arguments[2];
